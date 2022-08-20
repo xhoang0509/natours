1b675bc5dc76d6479e67b16f68e39841ad52bc8e
Hoang Nguyen Xuan <xhoang0509@gmail.com>
2022-08-20 09:37:49 +0700

    102. Aggregation Pipeline: Matching and Grouping

--- models/tourModel.js
+++ models/tourModel.js
@@ -15,7 +15,7 @@ const tourSchema = new mongoose.Schema({
     type: Number,
     required: [true, 'A tour must have a group size']
   },
-  ratingsAverge: {
+  ratingsAverage: {
     type: Number,
     default: 4.5
   },
