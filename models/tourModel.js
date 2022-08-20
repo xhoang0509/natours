1de7c070a4a11d1e625ac52afcb69f0d2b90b9bc
Hoang Nguyen Xuan <xhoang0509@gmail.com>
2022-08-20 16:16:46 +0700

    105. Document Middleware

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +9,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a durations']
@@ -63,6 +65,17 @@ const tourSchema = new mongoose.Schema(
     toObject: { virtuals: true }
   }
 );
+
+// DOCUMENT MIDLEWARE: runs before .save() and .create()
+tourSchema.pre('save', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+tourSchema.post('update', function(next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
 
 tourSchema.virtual('durationWeeks').get(function() {
   return this.duration / 7;
