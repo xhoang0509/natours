e62a61fd4814bb57f5752fbaa87523964aa8ff24
Hoang Nguyen Xuan <xhoang0509@gmail.com>
2022-08-27 16:35:03 +0700

    129. Signing up Users (JWT)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,11 +1,17 @@
+const jwt = require('jsonwebtoken');
 const User = require('./../models/userModel');
 const catchAsync = require('./../utils/catchAsync');
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
+  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SERCET, {
+    expiresIn: process.env.JWT_EXPIRES_IN.toString()
+  });
+
   res.status(201).json({
     status: 'success',
+    token,
     data: {
       user: newUser
     }
